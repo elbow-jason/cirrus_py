30c7d3bfe094a5392322ab920181e1ced1d4be7b
Jason Goldberger <jlgoldb2@asu.edu>
2017-04-24 12:32:39 -0700

    remove commented code

--- cirrus_py/__init__.py
+++ cirrus_py/__init__.py
@@ -1,13 +1,3 @@
 
-import cirrus_py.credentials #  as credentials
-import cirrus_py.google_jwt # as credentials
-
-# def main():
-#     """
-#     MAIN!
-#     """
-#     creds = credentials.from_service_account_json("./oh-stream-imager-credentials.secret.json")
-#     print("creds", creds)
-
-# if __name__ == '__main__':
-#     main()
+import cirrus_py.credentials
+import cirrus_py.google_jwt
