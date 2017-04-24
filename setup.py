f0cb276b0c2c8faf31dd12ba6f1bce0207ef3730
Jason Goldberger <jlgoldb2@asu.edu>
2017-04-24 12:33:06 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cirrus_py',
+    version='0.1.0',
+    description="Use Google Service Account JSON OAuth2 utility",
+    long_description=long_description,
+    url='https://github.com/elbow-jason/cirrus_py',
+    author='Jason Goldberger',
+    author_email='jlgoldb2@asu.edu',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='oauth oauth2 google-cloud',
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['PyJWT', 'requests'],
+)
